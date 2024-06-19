4e87e81fa57c114565c6a52654b085cdde870280
Oliver Tang <yifengt@outlook.com>
2024-06-19 16:12:28 -0500

    Reinstall flowmm

--- flowmm/src/flowmm/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Copyright (c) Meta Platforms, Inc. and affiliates."""
-
-try:
-    from ._version import version as __version__
-except ModuleNotFoundError:
-    __version__ = ""
--- flowmm/src/flowmm/data/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Copyright (c) Meta Platforms, Inc. and affiliates."""
-
-import math
-
-
-NUM_ATOMIC_TYPES: int = 100
-NUM_ATOMIC_BITS = math.floor(math.log2(NUM_ATOMIC_TYPES)) + 1
